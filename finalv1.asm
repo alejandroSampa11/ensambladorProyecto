da8816db6dfea621eff695a4ff5b00cee62b8e71
Alejandro Sampablo <alejandro200304@gmail.com>
2024-12-07 15:10:12 -0600

    menu2 y menu figuras

--- finalv1.asm
+++ finalv1.asm
@@ -20,6 +20,15 @@ ImprimirLinea MACRO fila, columna, caracter
 	mov al, caracter
 	int 10h
 ENDM
+;CAMBIOS
+LimpiarPantalla MACRO color
+	mov ah, 06h
+	mov al, 0
+	mov bh, color
+	mov cx, 0000h
+	mov dx, 184fh
+	int 10h
+ENDM
 
 .MODEL Large
 .STACK 100h
@@ -28,10 +37,21 @@ ENDM
 linea db '|$'
 separador db '----------------------BIENVENIDO--------------------$'
 separador2 db '----------------------------------------------------$'
+separador3 db '-------------------------MEN',00E9h,'------------------------$'
+separador4 db '-------------------------DIBUJAR FIGURAS------------------------$'
+actividades db 'SELECCIONE LA ACTIVIDAD QUE M',00E0h,'S LE GUSTE, DIVIERTASE!!!$'
+figuras db 'a) DIBUJAR FIGURAS$'
+figura1 db 'a) DIBUJAR TRIANGULO$'
+figura2 db 'b) DIBUJAR CUADRADO$'
+figura3 db 'c) DIBUJAR RECTANGULO$'
+colorear db 'b) COLOREAR A STEVE$'
+juego db 'c) JUGAR AL 3 EN RAYA$'
+salir db 'X) SALIR$'
 ale db 'Jos',82h,' Alejandro Sampablo Mart', 0A1h ,'nez$'
 daniel db 'Sergio Daniel Chiquito Zu', 00F1h ,'iga$'
 programa db 'Programa Tope de Power$'
 tecla db 'Pulse cualquier tecla para continuar...$'
+volver_menu db 'Z) VOLVER AL MEN',00E9h,' PRINCIPAL$'
 
 gatitoLinea1 db '     /\_/\        ', 0Dh, 0Ah, '$'
 gatitoLinea2 db '    ( o.o )      ', 0Dh, 0Ah, '$'
@@ -40,7 +60,7 @@ gatitoLinea4 db '    /     \     ', 0Dh, 0Ah, '$'
 gatitoLinea5 db '   /       \    ', 0Dh, 0Ah, '$'
 gatitoLinea6 db '   |       |   ', 0Dh, 0Ah, '$'
 gatitoLinea7 db '    \     /    ', 0Dh, 0Ah, '$'
-gatitoLinea8 db '     \___/     ', 0Dh, 0Ah, '$'
+gatitoLinea8 db '     \_/     ', 0Dh, 0Ah, '$'
 
 .CODE
 inicio:
@@ -50,12 +70,8 @@ mov ax, @data
 mov ds, ax
 
 	;CAMBIAR DE COLOR
-	mov ah, 06h
-	mov al, 0
-	mov bh, 57h
-	mov cx, 0000h
-	mov dx, 184fh
-	int 10h
+	LimpiarPantalla 57h
+
 	;MOVER EL CURSOR
 	Imprimir 8, 10, separador
 	Imprimir 10, 20, ale
@@ -88,20 +104,77 @@ loop_inicio_linea2:
 	ImprimirLinea dh,dl,'|'
 	inc dh
 loop loop_inicio_linea2
-
-	
+	;TECLA SOLICITADA
 	mov ah, 00h
 	int 16h
 	
+MENU2:
 	;CAMBIO DE PANTALLA
-	mov ah, 06h
-	mov al, 0
-	mov bh, 97h
-	mov cx, 0000h
-	mov dx, 184fh
-	int 10h
+	LimpiarPantalla 97h
+	Imprimir 8, 10, separador3
+	Imprimir 10, 9, actividades
+	Imprimir 12, 15, figuras
+	Imprimir 14, 15, colorear
+	Imprimir 16, 15, juego
+	Imprimir 18, 15, salir 
+	
+esperar_tecla:
+	mov ah, 00h
+	int 16h
+	cmp al, 'A';
+	je dibujar
+	cmp al, 'a'
+	je dibujar
+	cmp al, 'x'
+	je EXIT_CONTINUE
+	cmp al, 'X'
+	je EXIT_CONTINUE
+	jmp esperar_tecla
+	
+EXIT_CONTINUE:
+	jmp EXIT
+	
+dibujar:
+	LimpiarPantalla 47h
+	Imprimir 8,10, separador4
+	Imprimir 10,15, figura1
+	Imprimir 12,15, figura2
+	Imprimir 14,15, figura3
+	Imprimir 16,15, volver_menu
+esperar_tecla2:
+	mov ah, 00h
+	int 16h
+	cmp al, 'A'
+	je dibujarTriangulo
+	cmp al, 'a'
+	je dibujarTriangulo
+	cmp al, 'Z';
+	je MENU2_CONTINUE
+	cmp al, 'z'
+	je MENU2_CONTINUE
+	jmp esperar_tecla2
 
-	EXIT:
+dibujarTriangulo:
+	LimpiarPantalla 87h
+	mov ah, 00h
+	int 16h
+	JMP dibujar
+	
+	
+linea2:
+	inc dx
+	int 10h
+	cmp dx, 100
+	jne linea2
+	
+	mov ax, 03h
+	int 10h
+	jmp dibujar
+
+MENU2_CONTINUE:
+	jmp MENU2
+	
+EXIT:
 	mov AX, 4c00h
 	INT 21h
 END inicio
